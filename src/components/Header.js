97bbeddcf2fd35f90362899fbe635700733e65de
Danishhussain4271 <danishhussain4271@gmail.com>
2024-04-17 23:56:10 +0500

    inital push

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+// import React from 'react'
+// import Phone from '../Assets/images (1).jpeg';
+
+// function Header(props)
+// {
+//     console.warn(props.data)
+//     return(
+//         <div>
+//              <div className="add-to-cart">
+//     <span className="cart-count">{props.data.length}</span>
+//             <img src={Phone} alt='phone' />
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Header
---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Phone from '../Assets/images (1).jpeg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Home(props) {
+    return (
+        <div>
+           
+            <h1>Home Component</h1>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                    <img src={Phone} alt='phone' />
+                </div>
+                <div className="text-wrapper item">
+                    <span className="border-0">
+                        I-Phone
+                    </span>
+                    <span>
+                        Price: $1000.00
+                    </span>
+                </div>
+                <div className="btn-wrapper item ">
+                    <button 
+                    onClick={
+                        ()=>{props.addToCartHandler({pice:1000,name:'i phone 11'})}
+                        }>
+                        Add To Cart</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home
